f45f1b3f82aaab15ea7c9682501b053be8bd942e
Akarsh Agarwal <akarsh.agrawal2010@gmail.com>
2020-07-04 14:08:49 +0530

    Adding the initial app files.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import './tailwind.generated.css';
+
+const App: React.FC = () => (
+  <BrowserRouter>
+    <div> Hello </div>
+  </BrowserRouter>
+);
+
+export default App;
